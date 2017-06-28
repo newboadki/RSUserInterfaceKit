725586124d5592182ca382d7bb1e7713c3dca7d5
Borja Arias Drake <borja.arias@gmail.com>
2017-06-28 20:24:32 +0100

    Initial Commit

---
+++ RSUserInterfaceKit/RSUserInterfaceKit.h
@@ -0,0 +1,19 @@
+//
+//  RSUserInterfaceKit.h
+//  RSUserInterfaceKit
+//
+//  Created by Borja Arias Drake on 28/06/2017.
+//  Copyright © 2017 Borja Arias Drake. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RSUserInterfaceKit.
+FOUNDATION_EXPORT double RSUserInterfaceKitVersionNumber;
+
+//! Project version string for RSUserInterfaceKit.
+FOUNDATION_EXPORT const unsigned char RSUserInterfaceKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RSUserInterfaceKit/PublicHeader.h>
+
+
